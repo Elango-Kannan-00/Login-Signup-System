c6d84beb93c3c690dee1dfef613cd372fad432ed
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-12 18:35:21 +0530

    Add User entity class with JPA annotations for login-signup functionality

---
+++ src/main/java/com/example/login_signup/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.login_signup.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+}

5dcdafafd9b25ac680fee499003646ebc0165c5f
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-12 20:44:01 +0530

    Refactor User entity: change id field type to Integer and add Table annotation

--- src/main/java/com/example/login_signup/entity/User.java
+++ src/main/java/com/example/login_signup/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +15,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String userName;
     private String email;
     private String password;

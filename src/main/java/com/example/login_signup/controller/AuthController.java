009abcc033e9158bf453e7a7710d4e342e9e2245
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-12 20:13:06 +0530

    Refactor AuthController to use RestController and implement user registration, login, and retrieval endpoints

--- src/main/java/com/example/login_signup/controller/AuthController.java
+++ src/main/java/com/example/login_signup/controller/AuthController.java
@@ -1,8 +1,43 @@
 package com.example.login_signup.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.login_signup.dto.UserDetailsDto;
+import com.example.login_signup.dto.UserLoginDto;
+import com.example.login_signup.dto.UserRegistrationDto;
+import com.example.login_signup.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
 public class AuthController {
     
+    @Autowired
+    private UserService service;
+
+    // HTTP POST
+    @PostMapping("/register")
+    public String createUser(@Valid @RequestBody UserRegistrationDto user) {
+        return service.createUser(user);
+    }
+
+    // HTTP POST
+    @PostMapping("/login")
+    public String loginUser(@Valid @RequestBody UserLoginDto user) {
+        return service.loginUser(user);
+    }
+    
+    // HTTP GET
+    @GetMapping
+    public List<UserDetailsDto> getAllUsers() {
+        return service.getAllUsers();
+    }
 }

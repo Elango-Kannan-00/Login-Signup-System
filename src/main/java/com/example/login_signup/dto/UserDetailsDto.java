2871a4495155338168bdffcd5cef7f13b58ec056
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-12 20:13:10 +0530

    Add UserDetailsDto class for user information representation

---
+++ src/main/java/com/example/login_signup/dto/UserDetailsDto.java
@@ -0,0 +1,16 @@
+package com.example.login_signup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsDto {
+    private int id;
+    private String userName;
+    private String email;
+}

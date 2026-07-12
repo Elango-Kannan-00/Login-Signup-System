9303811c59ba228186daeed5f260d6f1c447e7f9
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-12 18:35:34 +0530

    Add UserRepository interface for user data access

---
+++ src/main/java/com/example/login_signup/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.login_signup.repository;
+
+import com.example.login_signup.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> { }

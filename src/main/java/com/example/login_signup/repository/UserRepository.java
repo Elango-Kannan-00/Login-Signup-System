eb6c8030259752488fea778c15414eedf899840b
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-12 20:13:24 +0530

    Add methods to UserRepository for email-based user retrieval and existence check

--- src/main/java/com/example/login_signup/repository/UserRepository.java
+++ src/main/java/com/example/login_signup/repository/UserRepository.java
@@ -5,4 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> { }
+public interface UserRepository extends JpaRepository<User, Integer> { 
+
+    // Find by email method.
+    User findByEmail(String email);
+
+    // Method for checking whether an user exist by email.
+    boolean existsByEmail(String email);
+}
